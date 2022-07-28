c31b5d734f0af19be5ebc6f70eb784b2a01930c0
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-07-28 20:28:25 -0300

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -3,3 +3,5 @@ env-update && source /etc/profile && export PS1="(chroot) ${PS1}";
 
 echo "Installing firmware and sources";
 emerge --ask sys-kernel/linux-firmware && emerge --ask sys-kernel/gentoo-sources;
+
+ 
