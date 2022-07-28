03c23a6cc70a16f332506d60824e77a1444a817f
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-07-28 19:45:47 -0300

    Update mount_disks.sh

--- mount_disks.sh
+++ mount_disks.sh
@@ -18,20 +18,4 @@ echo "Done! Activating swap...";
 swapon /dev/sda2;
 mkswap /dev/sda2;
 echo "Done! Mounting boot";
-mount /dev/sda3 /mnt/gentoo;
-echo "Done!";
-# to do: ask if it's correct, if not, ask hour minutes, day month and year
-echo "System date: $(date) (may be inaccurate)";
-echo "cd-ing to /mnt/gentoo";
-cd /mnt/boot;
-echo "Done! Please download stage3 openrc";
-
-links https://www.gentoo.org/downloads/mirrors/
-
-echo "Input any key to proceed... (press enter)";
-read u
-echo "What is the name of the stage3 file? (write 'stage' then press tab)";
-read stagename
-echo "Unpacking stage3...";
-tar xpvf $stagename --xattrs-include='*.*' --numeric-owner;
-echo "Done!";
+echo "Run mount_stage.sh";
